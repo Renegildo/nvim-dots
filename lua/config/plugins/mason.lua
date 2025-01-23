cf9ea56696a7b8fcb0a0e9f1d8be94a0a897ab7d
renegildo <renegildo@proton.me>
2025-01-22 22:47:27 -0300

    fix: remove tsserver on required servers

--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -10,7 +10,7 @@ return {
     dependencies = { "williamboman/mason.nvim", "neovim/nvim-lspconfig" },
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "tsserver", "lua_ls" },
+        ensure_installed = { "lua_ls" },
         automatic_installation = true,
       })
     end,
